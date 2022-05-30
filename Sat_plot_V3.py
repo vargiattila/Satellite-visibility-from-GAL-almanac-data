fd1ce5fbc54d3da6853b9ed35bf2e2324687b376
vargiattila <101498041+vargiattila@users.noreply.github.com>
2022-05-30 16:50:57 +0200

    Add files via upload

--- Sat_plot_V3.py
+++ Sat_plot_V3.py
@@ -6,8 +6,10 @@ import sys
 import gnsscal
 import datetime
 import numpy as np
+from datetime import datetime
 from itertools import takewhile, islice, dropwhile
 from numpy import linalg as LA
+
 pd.set_option('display.max_columns', None)
 
 
@@ -108,12 +110,12 @@ def read_xml(xml_file):
     sat_data['statusE1B'] = statusE1B
     sat_data = sat_data.astype(float)
     sat_data['ID'] = sat_data['ID'].astype(int)
-    sat_data['deltai'] = sat_data['deltai']*np.pi
+    sat_data['deltai'] = np.radians(56)+sat_data['deltai']*np.pi
     sat_data['omega0'] = sat_data['omega0']*np.pi
     sat_data['omegadot'] = sat_data['omegadot']*np.pi
     sat_data['w'] = sat_data['w']*np.pi
     sat_data['m0'] = sat_data['m0']*np.pi
-    sat_data['ax'] = (math.sqrt(29600000) + np.sqrt(sat_data['aSqRoot']))
+    sat_data['ax'] = (math.sqrt(29600000) + sat_data['aSqRoot'])**2
     return sat_data 
 
 def lla2ecef(lat, lon, alt):
@@ -141,7 +143,7 @@ def ymdhms2gps(year, month, mday, hour, minute, second):
     mjd = math.trunc(((year - 1901) / 4)) * 1461 + math.trunc(((year - 1901) % 4)) * 365 + yday - 1 + 15385
     fmjd = ((second / 60.0 + minute) / 60.0 + hour) / 24.0
     gps_week = math.trunc((mjd - 44244) / 7)
-    sec_of_week = ((mjd - 44244) - gps_week * 7 + fmjd) * 86400
+    sec_of_week = ((mjd - 44244) - gps_week * 7 + fmjd) * 86400+18
     return gps_week,sec_of_week
 
 def satpos(gps_week, sec_of_week, prn, health, ecc, ax, raw, aop, man, toa, inc, rra, week):
@@ -149,19 +151,15 @@ def satpos(gps_week, sec_of_week, prn, health, ecc, ax, raw, aop, man, toa, inc,
     Wedot = 7.2921151467e-5;	#WGS 84 value of earth's rotation rate
     mu =  3.986005e+14;		#WGS 84 value of earth's univ. grav. par.
     #mean motion
-    n = math.sqrt(mu/float(ax)**6);
-    #print(prn,n)
+    n = math.sqrt(mu/float(ax)**3);
     T = 2.0 * math.pi / n;
-    #print(T)
     dt = sec_of_week - float(toa);
-    #print(dt)
     if abs(dt) > 604800:
         print('*** to much time difference %f\n', dt);
     else:
         M = float(man) + n * dt;
         while (M<0):
             M = M + np.pi
-        #print(M)
     #Kepler equation
         E = M;
         Eold = 0.0;
@@ -170,35 +168,23 @@ def satpos(gps_week, sec_of_week, prn, health, ecc, ax, raw, aop, man, toa, inc,
             Eold = E
             E = M + float(ecc) * math.sin(E)
             j += 1
-            #print(prn,E)
     #true anomaly
         snu = math.sqrt(1.0-float(ecc)**2)*math.sin(E)
         cnu = math.cos(E)-float(ecc)
         nu = math.atan2(snu, cnu)
-        #print(nu)
+        #print('nu=',nu)
     #position in orbit plane
         u = nu+float(aop)
-        r = float(ax**2)*(1.0-float(ecc)*math.cos(E))
-        #print(r)
+        r = float(ax)*(1.0-float(ecc)*math.cos(E))
         wc = float(raw)+(float(rra)-Wedot)*dt-(float(toa)*Wedot) #felszálló csomópont
-        #print(wc)
-        xdash = r*math.cos(nu)
-        ydash = r*math.sin(nu)
+        xdash = r*math.cos(u)
+        ydash = r*math.sin(u)
         zdash = 0
         xsatorb = np.array([[xdash],[ydash],[zdash]])
-        #print(xsatorb)
     #position in ECEF system
-        Rz = np.array([[np.cos(aop), -np.sin(aop),0],\
-                      [np.sin(aop), np.cos(aop),0],\
-                      [0,0,1]]);
-        Rx = np.array([[1,0,0],\
-                      [0, np.cos(inc),-np.sin(inc)],\
-                      [0,np.sin(inc),np.cos(inc)]]);
-        Rzo = np.array([[np.cos(wc), -np.sin(wc),0],\
-                      [np.sin(wc), np.cos(wc),0],\
-                      [0,0,1]]);
-        xsat = Rzo.dot(Rx.dot(Rz.dot(xsatorb)))
-        xsat = list(xsat.flat)
+        xsat.append(float(xdash)*math.cos(float(wc)) - float(ydash)*math.cos(float(inc))*math.sin(float(wc)))
+        xsat.append(float(xdash)*math.sin(float(wc)) + float(ydash)*math.cos(float(inc))*math.cos(float(wc)))
+        xsat.append(float(ydash)*math.sin(float(inc)))
     return xsat
 
 def ECEF2GPS(Pos):
@@ -249,7 +235,7 @@ def Calc_Azimuth_Elevation(Pos_Rcv,Pos_SV):
     Pos_SV = np.array(Pos_SV)
     R=Pos_SV-Pos_Rcv;               ##vector from Reciever to Satellite
     GPS = ECEF2GPS(Pos_Rcv);        ##Lattitude and Longitude of Reciever
-    Lat=GPS[1];Lon=GPS[2];
+    Lat=GPS[0];Lon=GPS[1];
     ENU=XYZ2ENU(R,Lat,Lon);
     Elevation=np.arcsin(ENU[2]/LA.norm(ENU));
     Azimuth=np.arctan2(ENU[0]/LA.norm(ENU),ENU[1]/LA.norm(ENU));
@@ -259,28 +245,30 @@ def Calc_Azimuth_Elevation(Pos_Rcv,Pos_SV):
     A=np.degrees(Azimuth);
     return [E,A]
 
-
 ##import GALILEO almanach data
 sat_data = read_xml('2022-05-17.xml')
 #start time
 [gps_week, sec_of_week] = ymdhms2gps(2022,5,17,6,0,0)
 
 ##BUTE coordinates
-lat =np.radians(47.480944470876885)
-lon =np.radians(19.05652944792328)
+lat =np.radians(47.5)
+lon =np.radians(19)
 alt =0
 ##Station(/reciever) ECEF coordinates
 xsta = GPS2ECEF(lat,lon,alt);
 
 xsat = []
+cutoff = 10 #Elevation cutoff
+nsat = 0;
 for i in range(0,len(sat_data)):
     a = satpos(gps_week, sec_of_week, sat_data.iloc[i]['ID'], sat_data.iloc[i]['statusE5a'], sat_data.iloc[i]['ecc'], sat_data.iloc[i]['ax'], sat_data.iloc[i]['omega0'],\
         sat_data.iloc[i]['w'], sat_data.iloc[i]['m0'], sat_data.iloc[i]['t0a'], sat_data.iloc[i]['deltai'], sat_data.iloc[i]['omegadot'], sat_data.iloc[i]['wna'])
     xsat.append(a)
     AE = Calc_Azimuth_Elevation(xsta,xsat[i])
-    print('{0:.0f}'.format(sat_data.iloc[i]['ID']),AE)  
-
-
+    if AE[0]>cutoff:
+        print('{0:.0f}'.format(sat_data.iloc[i]['ID']),AE)
+        nsat +=1;
+print('Number of visible satellites at the mentioned moment:', nsat)
 
 
 
